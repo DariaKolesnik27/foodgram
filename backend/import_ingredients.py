548b08f8d8a8ed723415a3b071e22e04dad7b5ab
Daria Kolesnik <daria.gris@bk.ru>
2026-06-06 10:29:26 +0300

    Add ingredients

--- backend/import_ingredients.py
+++ backend/import_ingredients.py
@@ -1,74 +1,36 @@
-# import json
 import csv
 
 from django.conf import settings
+from django.core.management.base import BaseCommand
 
 from recipes.models import Ingredient
 
 
-# def load_from_json(filepath):
-#     print(f'Читаем JSON: {filepath}')
-#     with open(filepath, 'r', encoding='utf-8') as f:
-#         data = json.load(f)
+class Command(BaseCommand):
+    help = 'Импорт данных из CSV файла'
+    data_dir = settings.BASE_DIR / 'data'
 
-#     count = 0
-#     for item in data:
-#         name = item.get('name')
-#         unit = item.get('measurement_unit')
+    def handle(self, *args, **options):
+        try:
+            path = self.data_dir / 'ingredients.csv'
+            count = 0
+            with open(path, encoding='utf-8') as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    if len(row) >= 2:
+                        name = row[0].strip()
+                        unit = row[1].strip()
 
-#         if name and unit:
-#             obj, created = Ingredient.objects.get_or_create(
-#                 name=name,
-#                 measurement_unit=unit
-#             )
-#             if created:
-#                 count += 1
-#     print(f'Из JSON добавлено/обновлено: {count}')
-
-
-def load_from_csv_no_header(filepath):
-    """
-    Загружает из CSV БЕЗ заголовков.
-    Формат ожидается: название,единица (например: абрикосы,г)
-    """
-
-    try:
-        count = 0
-        with open(filepath, 'r', encoding='utf-8') as f:
-            reader = csv.reader(f)
-            for row in reader:
-                if len(row) >= 2:
-                    name = row[0].strip()
-                    unit = row[1].strip()
-
-                    if name and unit:
-                        obj, created = Ingredient.objects.get_or_create(
-                            name=name,
-                            measurement_unit=unit
-                        )
-                        if created:
-                            count += 1
-        print(f"[CSV] Успешно обработано: {count} записей.")
-    except FileNotFoundError:
-        print(f"[CSV] Файл не найден: {filepath}")
-    except Exception as e:
-        print(f"[CSV] Ошибка: {e}")
-
-
-if __name__ == '__main__':
-    json_path = '/data/ingredients.json'
-    csv_path = '/data/ingredients.csv'
-
-    # try:
-    #     load_from_json(json_path)
-    # except FileNotFoundError:
-    #     print(f'Файл {json_path} не найден. Пропускаем JSON.')
-    # except Exception as e:
-    #     print(f'Ошибка при чтении JSON: {e}')
-
-    try:
-        load_from_csv_no_header(csv_path)
-    except FileNotFoundError:
-        print(f'Файл {csv_path} не найден. Пропускаем CSV.')
-    except Exception as e:
-        print(f'Ошибка при чтении CSV: {e}')
+                        if name and unit:
+                            obj, created = Ingredient.objects.get_or_create(
+                                name=name,
+                                measurement_unit=unit
+                            )
+                            if created:
+                                count += 1
+            print(f"[CSV] Успешно обработано: {count} записей.")
+            self.stdout.write('Ingredient импортированы')
+        except FileNotFoundError:
+            print(f"[CSV] Файл не найден: {path}")
+        except Exception as e:
+            print(f"[CSV] Ошибка: {e}")
